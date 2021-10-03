ffc5d38a70d473fcd4735bf9dbf451ce0cceb0c5
shree7890 <shreekantadash831@gmail.com>
2021-10-03 11:01:20 +0600

    fdhfjdh

---
+++ src/components/Review/Review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div>
+            <h3>hello i am shreekanta dash my name id kantashree</h3>
+        </div>
+    );
+};
+
+export default Review;
