d5aaac8eda780737d77fc57b826e257a959a381b
shree7890 <shreekantadash831@gmail.com>
2021-10-06 22:46:50 +0600

    first me

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import Notfound from './components/Notfound/Notfound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+            <Notfound></Notfound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h4>404 error pager not found</h4>
+        </div>
+    );
+};
+
+export default Notfound;
